4e0775062e1a7defc1cd0c571429d519a9e66646
Georgi Murdzheff <120983342+murdzheff@users.noreply.github.com>
2025-05-05 23:31:03 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,9 +8,9 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+dnf5 group list --hidden
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux %cinnamon-session
 
 # Use a COPR Example:
 #
