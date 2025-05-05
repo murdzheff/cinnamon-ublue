067c1b7abf4069330a002130d4b3a87e15a99560
Georgi Murdzheff <120983342+murdzheff@users.noreply.github.com>
2025-05-05 23:42:37 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 dnf5 group list --hidden
+dnf5 group info "cinnamon-desktop"
+
 # this installs a package from fedora repos
 dnf5 install -y tmux
-dnf install %cinnamon-desktop -y
+dnf5 group install "cinnamon-desktop"
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
